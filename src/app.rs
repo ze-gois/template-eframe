7aa4c027136b9457874c9449a8d7b074ed3add16
José Gois <jhenriquetdg@gmail.com>
2024-09-04 00:41:30 -0300

    eframe hello world

--- src/app.rs
+++ src/app.rs
@@ -67,6 +67,7 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("Hello, World");
             ui.heading("eframe template");
 
             ui.horizontal(|ui| {
